86dd4ec78b952384f32f40325297352c050b4be2
Robert Seney <robert.seney@kearsarge.org>
2024-02-21 08:34:57 -0500

    changed turtle armor and bald eagle images, added collision handler file

---
+++ Collisionhandler.py
@@ -0,0 +1,24 @@
+from math import * 
+#                               **ANYBODY WORKING ON THIS DOCUMENT, READ BELOW**    
+#For inelastic cololisions
+#momentum formula, as momentum is convserved in all linear collisions. There wil be no angular collisions becasue I am not adept with those.
+#   m(10)v(10)+m(20)v(10)=m(1+2)v(1)
+#we do not need kinetic energy for e\inelastic collisions. 
+#
+#For elastic collisions:
+#momentum formula, as momentum is conserved in all linear collisions
+#   m(10)v(10)+m(20)v(20)=m(11)v(11)=m(21)v(21)
+#Kinetic energy formula, as kinetic energy is conserved ni elastic collisions. this is important because both velocities will change after the collision and we need to form a system of equations to find both.
+#If you chgange anything, one must substitute into the other. Good Luck.
+#   K=(mv^2)/2
+#Kinetic energy must transfer between objects, which means that all energy levels will be diffferent before and after the collision.
+
+#you will know the math works if, when you run a inelastic collision, the objects both have the same velociy after
+#                                              an  elastic collision, the objects have separate velocities, not necessarily traveling antiparallel.
+#               have fun lmao
+
+def elasticCollision:
+     
+
+def inelasticCollision:
+    
