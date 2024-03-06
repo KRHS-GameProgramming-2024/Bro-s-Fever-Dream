5fda617d511ed26e30d7cd371738b413bd812657
Camden Blasingame <camden.blasingame@gmail.com>
2024-03-06 07:42:28 -0500

    More Music, working on getting loops and transitions clean

--- Music/JukeBox.py
+++ Music/JukeBox.py
@@ -2,8 +2,9 @@ import pygame
 pygame.init()
 pygame.mixer.init()
 
+print("1: Cave Theme \n2: Final Boss\n3: Final Boss 2nd Phase\n4: Sad Theme\n5: Starter Enemy Theme\n6: The Lava")
+
 while True:
-	print("1: Cave Theme \n2: Final Boss\n3: Final Boss 2nd Phase\n4: Sad Theme\n5: Starter Enemy Theme\n6: The Lava")
 	number = input("Press a number: ")
 
 
@@ -14,9 +15,7 @@ while True:
 	elif number == "2":
 		print("Final Boss")
 		pygame.mixer.music.load("FinalBoss.mp3")
-		pygame.mixer.music.play()
-		pygame.mixer.music.queue("FinalBossLoop.mp3")
-		pygame.mixer.music.play(-1)	
+		pygame.mixer.music.play(-1)
 	elif number == "3":
 		print("Final Boss 2nd Phase")
 		pygame.mixer.music.load("CaveTheme.mp3")
