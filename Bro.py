bd3bb3d82a2769fa411aa734f043541bc4e2e3ee
Robert Seney <robert.seney@kearsarge.org>
2024-03-13 08:03:15 -0400

    it didn't look like worked

--- Bro.py
+++ Bro.py
@@ -2,8 +2,8 @@ import math, pygame, sys, random
 	
 class Bro():
     def __init__(self, maxSpeed=4, speed = [8,8], startPos=[0,1]):
-        self.images = [pygame.image.load("Bro/Images/BroLeft.png"),
-                        pygame.image.load("Bro/Images/BroRight.png")]
+        self.images = [pygame.image.load("Bro/Images/BroLeftBigger.png"),
+                        pygame.image.load("Bro/Images/BroRightBigger.png")]
         self.frame = 0
         self.frameMax = len(self.images)-1
         self.image = self.images[self.frame]  
@@ -99,5 +99,4 @@ class Bro():
         self.didBounceY = False
         self.wallCollide(size)
         self.fall(size)
-        print("speed in Y direction " + str(self.speedy))
         
