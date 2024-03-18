4f59747c028731ebe60ad589a38fbc4de91057be
Camden Blasingame <camden.blasingame@gmail.com>
2024-03-18 09:02:24 -0400

    Trying to have wall collision

--- Bro.py
+++ Bro.py
@@ -2,69 +2,55 @@ import math, pygame, sys, random
 from Enemy import *
 	
 class Bro(Charter):
-    def __init__(self, maxSpeed=4, speed = [8,8], startPos=[0,1]):
-        Charter.__init__(self, [0,0], startPos)
-        self.images = [pygame.image.load("Bro/Images/BroLeftBigger.png"),
-                        pygame.image.load("Bro/Images/BroRightBigger.png")]
-        self.frame = 0
-        self.frameMax = len(self.images)-1
-        self.image = self.images[self.frame]  
-        self.rect = self.image.get_rect()
-        self.rect = self.rect.move(startPos)
-        self.maxSpeed = maxSpeed
-        self.speedx = speed[0]
-        self.speedy = speed[1]
-        self.speed = [self.speedx, self.speedy]
-        self.kind = "Bro"
-            
-        self.didBounceX = False
-        self.didBounceY = False
-        
+	def __init__(self, maxSpeed=4, speed = [8,8], startPos=[0,1]):
+		Charter.__init__(self, [0,0], startPos)
+		self.images = [pygame.image.load("Bro/Images/BroLeftBigger.png"),
+						pygame.image.load("Bro/Images/BroRightBigger.png")]
+		self.frame = 0
+		self.frameMax = len(self.images)-1
+		self.image = self.images[self.frame]  
+		self.rect = self.image.get_rect()
+		self.rect = self.rect.move(startPos)
+		self.maxSpeed = maxSpeed
+		self.speedx = speed[0]
+		self.speedy = speed[1]
+		self.speed = [self.speedx, self.speedy]
+		self.kind = "Bro"
+			
+		self.didBounceX = False
+		self.didBounceY = False
+		
    
-                
-    def goKey(self, direction):
-        if direction == "left":
-            self.speedx = -self.maxSpeed
-        elif direction == "right":
-            self.speedx = self.maxSpeed
-        elif direction == "up" and not self.jumping:
-            self.jumping = True
-            self.speedy = -self.jumpHeight
-        elif direction == "down":
-            self.speedy = self.maxSpeed
-        elif direction == "sleft":
-            self.speedx = 0
-        elif direction == "sright":
-            self.speedx = 0
-        elif direction == "sup":
-            # ~ self.speedy = 0
-            pass
-        elif direction == "sdown":
-            self.speedy = 0
-    
-    def look(self, look):
-        if look == "right":
-            self.frame = 1
-        elif look == "left":
-            self.frame = 0
-        self.image = self.images[self.frame]
-        
-                
-    def wallsCollide(self, other):
-        if self.rect.right > other.rect.left:
-            if self.rect.left < other.rect.right:
-                if self.rect.bottom > other.rect.top:
-                    if self.rect.top < other.rect.bottom:
-                        if not self.didBounceX:
-                            self.speedx = -self.speedx
-                            self.didBounceX = True
-                        if not self.didBounceY:
-                            self.speedy = -self.speedy
-                            self.didBounceY = True
-                            return True
-                        pass
+				
+	def goKey(self, direction):
+		if direction == "left":
+			self.speedx = -self.maxSpeed
+		elif direction == "right":
+			self.speedx = self.maxSpeed
+		elif direction == "up" and not self.jumping:
+			self.jumping = True
+			self.speedy = -self.jumpHeight
+		elif direction == "down":
+			self.speedy = self.maxSpeed
+		elif direction == "sleft":
+			self.speedx = 0
+		elif direction == "sright":
+			self.speedx = 0
+		elif direction == "sup":
+			# ~ self.speedy = 0
+			pass
+		elif direction == "sdown":
+			self.speedy = 0
+	
+	def look(self, look):
+		if look == "right":
+			self.frame = 1
+		elif look == "left":
+			self.frame = 0
+		self.image = self.images[self.frame]
 
 
-    
-        
-        
+
+	
+		
+		
--- Main.py
+++ Main.py
@@ -20,47 +20,51 @@ walls = tiles
 player = Bro(8, [0,0], [1024/2, 768/2])
 Bros = [player]
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit();
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                player.goKey("left")
-                player.look("left")
-            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                player.goKey("right")
-                player.look("right")
-            elif event.key == pygame.K_w or event.key == pygame.K_UP:
-                player.goKey("up")
-                print(player.jumping, player.speedy)
-            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                player.goKey("down")
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                player.goKey("sleft")
-            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                player.goKey("sright")
-            elif event.key == pygame.K_w or event.key == pygame.K_UP:
-                player.goKey("sup")
-            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                player.goKey("sdown")
-    
-    counter += 1
-    print(counter)
-    if counter % 100 == 0:
-        Bros += [GloopyGlob()]
-    for Charter in Bros:
-        Charter.update(size)
-    #print(player.jumping, player.speedy)
-        
-    screen.fill((255, 255, 255))
-    for Bro in Bros:
-        screen.blit(Bro.image, Bro.rect)
-    for wall in walls:
-        screen.blit(wall.image, wall.rect)
-    pygame.display.flip()
-    Clock.tick(60);
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit();
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+				player.goKey("left")
+				player.look("left")
+			elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+				player.goKey("right")
+				player.look("right")
+			elif event.key == pygame.K_w or event.key == pygame.K_UP:
+				player.goKey("up")
+				print(player.jumping, player.speedy)
+			elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+				player.goKey("down")
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+				player.goKey("sleft")
+			elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+				player.goKey("sright")
+			elif event.key == pygame.K_w or event.key == pygame.K_UP:
+				player.goKey("sup")
+			elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+				player.goKey("sdown")
+	
+	counter += 1
+	print(counter)
+	if counter % 100 == 0:
+		Bros += [GloopyGlob()]
+	for Charter in Bros:
+		Charter.update(size)
+	#print(player.jumping, player.speedy)
+	
+	for Collision in Bros:
+		for wall in walls:
+			Collision.wallTileCollide(wall)
+		
+	screen.fill((255, 255, 255))
+	for Bro in Bros:
+		screen.blit(Bro.image, Bro.rect)
+	for wall in walls:
+		screen.blit(wall.image, wall.rect)
+	pygame.display.flip()
+	Clock.tick(60);
    
-    #print(Clock.get_fps())
+	#print(Clock.get_fps())
 
 
