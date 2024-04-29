9f774737600ee9c169f5fb9bba267b92668fb4b9
Lilly <Lilly.Wolfinger@kearsarge.org>
2024-04-29 08:07:45 -0400

    levels

--- Levels/Walls.py
+++ Levels/Walls.py
@@ -4,8 +4,8 @@ class Wall():
 	def __init__(self,  pos=[25,2]):
 		self.image = pygame.image.load("../Blocks/Dirt/Images/Dirt.png")
 		self.rect = self.image.get_rect(center = pos)
-		
+
 		self.kind = "wall"
-			
+
 	def update(self, size):
 		pass
