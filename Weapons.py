2565b283bda233ae3b95f2d409a8907031dc9556
Robert Seney <robert.seney@kearsarge.org>
2024-05-15 08:49:46 -0400

    weapon finally rotates, added sounds for first shooter, working on first shooter

--- Weapons.py
+++ Weapons.py
@@ -70,14 +70,6 @@ class dagger:
         # ~ print("Movement: " + str([self.movementX, self.movementX]))
         return self.change
         
-    def rot_center(self):
-        """rotate an image while keeping its center and size"""
-        orig_rect = self.image.get_rect()
-        rot_image = pygame.transform.rotate(self.image, 180*self.facing/math.pi)
-        rot_rect = orig_rect.copy()
-        rot_rect.center = rot_image.get_rect().center
-        rot_image = rot_image.subsurface(rot_rect).copy()
-        self.image = rot_image
             
     def update(self, player):
         if (3 * math.pi / 2) > self.facing > (math.pi / 2):
@@ -133,6 +125,84 @@ class sword:
 
 # ~ class staff:
     # ~ pass
-# ~ class shooter:
+# ~ class shield:
     # ~ pass
+class shooter:
+    def __init__(self, useSpeed = 1, 
+                   startPos = [0, 1], 
+                   angle = int(pi/3), 
+                   name = "Soup Ladle",  
+                   images = [pygame.image.load("Earth/SoupLadle/Images/SoupLadleLeft.png"), 
+                            pygame.image.load("Earth/SoupLadle/Images/SoupLadleRight.png")
+                            ]):
+        self.images = [pygame.image.load("Earth/SoupLadle/Images/SoupLadleLeft.png"), pygame.image.load("Earth/SoupLadle/Images/SoupLadleRight.png")]
+        self.facing = angle
+        self.frame = 1
+        print(self.images)
+        self.image = self.images[self.frame]  
+        self.rect = self.image.get_rect(center = startPos)
+        #self.rect = self.rect.animate(startPos)
+        self.useSpeed = useSpeed
+        self.movementX = 0
+        self.movementY = 0
+        self.movement = [self.movementX, self.movementY]
+        self.change = 1
+        self.mass = .01
+        self.damage = 9999
+        adjacent = 100
+        self.kind = "Popper"
+        self.speedy = 0
+        self.speedx = 0
+        self.playerSpeedX = 0
+        self.playerSpeedY = 0
+        self.speedScaler = 2
+        self.timeScaler = 3
+        self.effectStart = pygame.mixer.Sound("Air/BubbleLauncher/Sounds")
+        self.effectContinue = pygame.mixer.Sound("Air/BubbleLauncher/Sounds")
+        self.specialeffectStart = pygame.mixer.Sound("Air/BubbleLauncher/Sounds")
+        self.specialeffectContinue = pygame.mixer.Sound("Air/BubbleLauncher/Sounds")
+        
+    def move(self):
+        self.image = pygame.transform.rotate(self.image, 315 - (180 * self.facing / math.pi))
+        self.movement = [self.playerSpeedX, self.playerSpeedY]
+        # ~ print("movement trying to go, movement = ", self.movement)
+        self.rect = self.rect.move(self.movement)
+        # ~ print("Rect: ", self.rect)
+        
+    def animate(self):
+        self.effectStart.play()
+        if self.live == 1:
+            self.image = pygame.transform.rotate(self.image, 270 - (180 * self.facing / math.pi))
+            self.previousRotation = 270 - (180 * self.facing / math.pi)
+            self.pin = True
+        else: 
+            self.image = pygame.transform.rotate(self.image, 270 - (180 * self.facing / math.pi) - self.previousRotation)
+            self.previousRotation = 270 - (180 * self.facing / math.pi) - self.previousRotation
+            self.pin = True
+        
+    def update(self, player):
+        if (3 * math.pi / 2) > self.facing > (math.pi / 2):
+            self.frame = 0
+        else:
+            self.frame = 1
+        adjacent = 1
+        self.live += 1
+        # ~ print("Mouse Pos: " + str(pygame.mouse.get_pos()) + ", Player Pos: " + str(player.rect.center))
+        diff = [(pygame.mouse.get_pos()[0] - player.rect.center[0]), (pygame.mouse.get_pos()[1] - player.rect.center[1])]
+        adjacent = diff[0]
+        if adjacent == 0:
+            adjacent = .01
+    
+        self.facing = atan(diff[1] / adjacent)
+        if (diff[0] < 0):
+            self.facing += math.pi
+        # ~ print("Facing: ", self.facing, "Adjacent: ", adjacent)
+        if player.speedy != self.playerSpeedY:
+            self.playerSpeedY = player.speedy
+            self.speedy += self.playerSpeedY
+        if player.speedx != self.playerSpeedX:
+            self.playerSpeedX = player.speedx
+            self.speedx += self.playerSpeedX 
+        self.animate
+        self.move()
     
