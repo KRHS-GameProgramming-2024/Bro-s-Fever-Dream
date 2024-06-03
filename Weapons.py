98c88b23c39b300a5eca825846e0698e1181ce8d
Robert Seney <robert.seney@kearsarge.org>
2024-06-03 07:50:20 -0400

    added boomerang

--- Weapons.py
+++ Weapons.py
@@ -256,3 +256,128 @@ class shooter:
 
 class projectile:
     pass
+class boomerang:
+    def __init__(self, useSpeed = 1, 
+                   startPos = [0, 1], 
+                   angle = int(pi/3), 
+                   name = "Soup Ladle",  
+                   images = [pygame.image.load("Earth/SoupLadle/Images/SoupLadleLeft.png"), 
+                            pygame.image.load("Earth/SoupLadle/Images/SoupLadleRight.png")
+                            ]):
+        self.images = []
+        for image in images:
+            self.images += [pygame.image.load(image)]
+        self.facing = angle
+        self.frame = 0
+        print(self.images)
+        self.image = self.images[self.frame]  
+        self.rect = self.image.get_rect(center = startPos)
+        #self.rect = self.rect.animate(startPos)
+        self.useSpeed = useSpeed
+        self.movementX = 0
+        self.movementY = 0
+        self.movement = [self.movementX, self.movementY]
+        self.change = 1
+        self.mass = 1
+        self.damage = 9999
+        adjacent = 100
+        self.kind = "Boomerang"
+        self.speedy = 0
+        self.speedx = 0
+        self.playerSpeedX = 0
+        self.playerSpeedY = 0
+        self.live = 0
+        
+        self.speedScaler = 2
+        self.timeScaler = 3
+        
+    def move(self):
+        self.movement = [round(self.speedx), round(self.speedy)]
+        # ~ print("movement trying to go, movement = ", self.movement)
+        self.rect = self.rect.move(self.movement)
+        # ~ print("Rect: ", self.rect)
+        
+                
+    def animate(self):
+        print("animate is called")
+        
+        if self.change == 1:
+            self.speedx = 5 * self.speedScaler * (cos(self.facing)) + self.playerSpeedX * 2 + 1 * (cos(self.facing))
+            self.speedy = 5 * self.speedScaler * (sin(self.facing)) + self.playerSpeedY + 1 * (sin(self.facing))
+        if self.change == 2:
+            self.speedx = 4 * self.speedScaler * (cos(self.facing)) + self.playerSpeedX * 2 + 2 * (cos(self.facing))
+            self.speedy = 4 * self.speedScaler * (sin(self.facing)) + self.playerSpeedY + 2 * (sin(self.facing))
+        if self.change == 3:
+            self.speedx = 3 * self.speedScaler * (cos(self.facing)) + self.playerSpeedX * 2 + 3 * (cos(self.facing))
+            self.speedy = 3 * self.speedScaler * (sin(self.facing)) + self.playerSpeedY + 3 * (sin(self.facing))
+        if self.change == 4:
+            self.speedx = -3 * self.speedScaler * (cos(self.facing)) + self.playerSpeedX * 2 - 1 * (cos(self.facing))
+            self.speedy = -3 * self.speedScaler * (sin(self.facing)) + self.playerSpeedY - 1 * (sin(self.facing))
+        if self.change == 5:
+            self.speedx = -4 * self.speedScaler * (cos(self.facing)) + self.playerSpeedX * 2 - 2 * (cos(self.facing))
+            self.speedy = -4 * self.speedScaler * (sin(self.facing)) + self.playerSpeedY - 2 * (sin(self.facing))
+        if self.change == 6:
+            self.speedx = -5 * self.speedScaler * (cos(self.facing)) + self.playerSpeedX * 2 - 3 * (cos(self.facing))
+            self.speedy = -5 * self.speedScaler * (sin(self.facing)) + self.playerSpeedY - 3 * (sin(self.facing))
+        self.change += 1/self.timeScaler
+        # ~ print(self.change)
+        # ~ print("sine of angle ", sin(self.facing), " cosine of angle ", cos(self.facing), " self.facing ", self.facing)
+        # ~ print("Movement: " + str([self.movementX, self.movementX]))
+        return self.change
+        
+            # ~             self.image = pygame.transform.rotate(self.image, 315 - (180 * self.facing / math.pi))
+    def update(self, player):
+        if (3 * math.pi / 2) > self.facing > (math.pi / 2):
+            self.frame = 0
+        else:
+            self.frame = 1
+        
+        adjacent = 1
+        self.live += 1
+        #self.image = pygame.transform.rotate(self.image, 315 - (15 / math.pi))
+        # ~ print("Mouse Pos: " + str(getScaledMouse()) + ", Player Pos: " + str(player.rect.center))
+        diff = [(getScaledMouse()[0] - player.rect.center[0]), (getScaledMouse()[1] - player.rect.center[1])]
+        adjacent = diff[0]
+        if adjacent == 0:
+            adjacent = .01
+    
+        self.facing = atan(diff[1] / adjacent)
+        if (diff[0] < 0):
+            self.facing += math.pi
+        # ~ print("Facing: ", self.facing, "Adjacent: ", adjacent)
+
+
+        self.animate()
+        if self.live >= self.timeScaler/2:
+            self.speedx += player.speedx * 2
+            self.speedy += player.speedy * 2
+        else:
+            self.speedy -= self.playerSpeedY
+            self.playerSpeedY = player.speedy * 2
+            self.speedy += self.playerSpeedY
+            self.speedx -= self.playerSpeedX
+            self.playerSpeedX = player.speedx * 2
+            self.speedx += self.playerSpeedX
+
+        
+        # ~ if self.live == 1:
+            # ~ self.speedx += player.speedx
+            # ~ self.speedy += player.speedy
+        # ~ print("-------> was: ", self.speedx, self.speedy)
+        # ~ if player.speedy != self.playerSpeedY:
+            # ~ self.speedy -= self.playerSpeedY
+            # ~ self.playerSpeedY = player.speedy
+            # ~ self.speedy += self.playerSpeedY
+        # ~ if player.speedx != self.playerSpeedX:
+            # ~ self.speedx -= self.playerSpeedX
+            # ~ self.playerSpeedX = player.speedx
+            # ~ self.speedx += self.playerSpeedX
+        # ~ print("-------> now: ", self.speedx, self.speedy)
+        self.playerSpeedX = player.speedx
+        self.playerSpeedY = player.speedy
+        self.animate()
+        self.move()
+
+                
+            
+                
